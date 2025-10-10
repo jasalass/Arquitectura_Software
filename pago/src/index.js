0b66461ccc984fd6e37c223e561516eedc23bb9d
Franco <zzapata259@gmail.com>
2025-10-09 22:09:14 -0300

    Creacion modulo pago

---
+++ pago/src/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import dotenv from "dotenv";
+
+//Configuración de dotenv
+dotenv.config();
+
+//Iniciar express
+const app = express();
+
+//Asignar puerto
+const PORT = process.env.PORT || 6000;
+
+//Middleware para JSON
+app.use(express.json());
+
+// Middleware para datos tipo application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//Endpoint de prueba
+app.get('/', (req, res) =>{
+    res.json({message:'API Pago funcionando'})
+});
+
+//Endpoint de autenticación
+/*app.post('/auth', (req, res) =>{
+    let email = req.body.email;
+    let password = req.body.password;
+    
+    if (email === "user@email.com" && password === "12345"){
+        
+        // Éxito 200 OK
+        res.status(200).json({ success: true, message: "Login correcto" });
+        
+    } else {
+        //Error 401 sin autorización
+        res.status(401).json({success: false, message: "Credenciales Inválidas"});
+    }
+});*/
+
+//Healthcheck (para kubernetes)
+app.get('/healthz', (req,res)=>{res.sendStatus(200)});
+app.get('/ready', (req,res)=>{res.sendStatus(200)});
+
+app.listen(PORT, () =>{
+    console.log(`Pago corriendo en http://localhost:${PORT}`)
+});
