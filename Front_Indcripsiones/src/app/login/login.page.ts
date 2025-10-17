ba0df0317c8d7b258750673673663a65dcd3f532
JuanAndresSalas <juan.salas.salas@outlook.com>
2025-10-17 03:13:12 -0300

    feat(docker): agregar build de producción para frontend con Nginx
    - Implementado Dockerfile multietapa:
      - Etapa 1: build de Ionic/Angular con Node 20 (npm ci + ng build)
      - Etapa 2: despliegue con Nginx 1.27 Alpine
    - Se agregó configuración de Nginx para soportar rutas SPA (Angular)
    - Ajustado manejo de permisos y PID para ejecución sin root (USER 1001)
    - Incluido .dockerignore para reducir tamaño del contexto
    - Se probó ejecución con docker run -p 8090:80 front-inscripciones:prod funcionando correctamente

--- Front_Indcripsiones/src/app/login/login.page.ts
+++ Front_Indcripsiones/src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonCol, IonGrid, IonRow, IonButton, IonInputPasswordToggle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonCol, IonGrid, IonRow, IonButton, IonInputPasswordToggle, IonInput } from '@ionic/angular/standalone';
 import { Autentificacion } from '../Service/autentificacion';
 import { Router } from '@angular/router';
 
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonLabel, IonCol, IonGrid, IonRow, IonButton, IonInputPasswordToggle]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonLabel, IonCol, IonGrid, IonRow, IonButton, IonInputPasswordToggle, IonInput]
 })
 export class LoginPage implements OnInit {
 
@@ -19,11 +19,12 @@ export class LoginPage implements OnInit {
 
   constructor(private auth: Autentificacion, private router: Router) {}
 
-   submit() {
+    submit() {
     this.auth.login(this.email, this.password).subscribe({
       next: res => {
         if (res.success) {
-          alert('Login exitoso');
+          alert('Login Exitoso');
+          
           this.router.navigateByUrl('/home');
         } else {
           alert('Credenciales inválidas');
