72751ff281a8d34abbccdee46d042c57ac55f62a
Vina del Mar <vina@duoc.acad>
2025-10-21 20:29:22 -0300

    Avance

--- Front_Inscripciones/src/app/pago-matricula/pago-matricula.page.ts
+++ Front_Inscripciones/src/app/pago-matricula/pago-matricula.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
@@ -10,11 +10,28 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class PagoMatriculaPage implements OnInit {
+export class PagoMatricula {
 
   constructor() { }
-
   ngOnInit() {
   }
+  
+  rut: string = '';
+  estado: string | null = null;
 
+  buscarEstado() {
+    // 🔹 Por ahora es una simulación simple.
+    // Más adelante puedes reemplazar esto por una llamada al backend (HttpClient)
+    if (this.rut === '12.345.678-9') {
+      this.estado = 'PREINSCRITA';
+    } else if (this.rut === '11.111.111-1') {
+      this.estado = 'INSCRITA';
+    } else {
+      this.estado = 'No encontrado';
+    }
+  }
 }
+
+
+
+
